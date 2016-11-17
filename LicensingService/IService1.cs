b6cf5b90326d81b5b1f57237331e0e471418c617
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-11-17 09:28:36 +0100

    Cleanup

--- LicensingService/IService1.cs
+++ LicensingService/IService1.cs
@@ -1,32 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
+﻿ using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
-
+ 
 namespace LicensingService {
-  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
-
-    /*
-     * product id
-     * users
-     * features
-     * databases
-     * email
-     * generate rnd num on each number
-     */
-  [ServiceContract]
+ [ServiceContract]
   public interface IService1 {
 
-    [OperationContract]
-    string GetData(int value);
-
-    [OperationContract]
-    CompositeType GetDataUsingDataContract(CompositeType composite);
-
-    [OperationContract]
+ [OperationContract]
     string GetNewSerial( );
 
     [OperationContract]
@@ -37,8 +16,6 @@ namespace LicensingService {
 
     [OperationContract]
     bool AddSubscription(SubscribeViewModel model);
-
-    // TODO: Add your service operations here
   }
 
   [DataContract]
@@ -59,22 +36,5 @@ namespace LicensingService {
     public string Serial { get; set; }
   }
 
-  // Use a data contract as illustrated in the sample below to add composite types to service operations.
-  [DataContract]
-  public class CompositeType {
-    bool boolValue = true;
-    string stringValue = "Hello ";
-
-    [DataMember]
-    public bool BoolValue {
-      get { return boolValue; }
-      set { boolValue = value; }
-    }
-
-    [DataMember]
-    public string StringValue {
-      get { return stringValue; }
-      set { stringValue = value; }
-    }
-  }
+ 
 }
--- LicensingService/Service1.svc.cs
+++ LicensingService/Service1.svc.cs
@@ -66,20 +66,6 @@ namespace LicensingService {
       }
       return true;
     }
-
-
-    public string GetData(int value) {
-      return string.Format("You entered: {0}", value);
-    }
-
-    public CompositeType GetDataUsingDataContract(CompositeType composite) {
-      if (composite == null) {
-        throw new ArgumentNullException("composite");
-      }
-      if (composite.BoolValue) {
-        composite.StringValue += "Suffix";
-      }
-      return composite;
-    }
+ 
   }
 }
